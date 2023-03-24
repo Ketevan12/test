02afcf6390044fee22977755054b8840ff7ab9db
Ketevan12 <kirvk209@newschool.edu>
2023-03-24 18:18:06 -0400

    g

---
+++ script.js
@@ -0,0 +1,56 @@
+script.js
+const albumsList = document.getElementById('albums-list')
+
+function renderAlbum(album, i) {
+  // Create .album element
+  const albumElement = document.createElement('div')
+
+  albumElement.classList.add('album')
+  albumElement.classList.add(`album-${i}`)
+
+  console.log(albumElement)
+  console.log(album.cover_image_url)
+
+  // Create the inner HTML content
+  const albumHTML = `
+    <a href="albumDetail.html?title=${album.title}">
+      <img src="${album.cover_image_url}">
+    </a>
+    <ul>
+      <li>Title: ${album.title}</li>
+      <li>Artist: ${album.artist}</li>
+      <li>Released: ${album.year_released}</li>
+      <li>Tracks: ${album.total_tracks}</li>
+      <li>Genre: ${album.genre}</li>
+      <li>Added By: ${album.added_by}</li>
+      <li>
+        <a href="${album.listen_url}">
+         Listen Here
+        </a>
+      <li>
+    </ul>
+  `
+
+  // Set the inner HTML content of the albumElement
+  albumElement.innerHTML = albumHTML
+
+  // Add to the list #albums-list
+  albumsList.appendChild(albumElement)
+}
+
+// Start App!
+// albums.forEach(function(album) {
+//   // if (album.year_released < 2000) {
+//     renderAlbum(album)
+//   // }
+// })
+
+fetch('albums.json')
+    .then((response) => response.json())
+    .then((json) => {
+      json.albums.forEach(function(album, i) {
+        // if (album.year_released < 2000) {
+          renderAlbum(album, i)
+        // }
+      })
+    });
